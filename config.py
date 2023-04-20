1e772fa78fc73bbcf529f6fe1a18a8ce8b22b704
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2023-04-20 13:26:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/shraajestayu/palakXesbot",
+    "https://github.com/shraajestayu/esbot-x2.0",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
