7ec01c2c565c6d104a332a275250b32de21aa73e
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2023-05-15 16:47:04 +0530

    Update mongo.py
    - Old_Database_Loaded

--- AnonX/core/mongo.py
+++ AnonX/core/mongo.py
@@ -8,5 +8,5 @@ from ..logging import LOGGER
 
 _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
 _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-mongodb = _mongo_async_.estella
-pymongodb = _mongo_sync_.estella
+mongodb = _mongo_async_.Yukki
+pymongodb = _mongo_sync_.Yukki
